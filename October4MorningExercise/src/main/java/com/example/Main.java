0c5ef8aef1e9e6244555f60378d02158fbe94811
Supriya Roberson <supriyaroberson@Supriyas-MBP.home>
2023-10-04 09:20:16 -0500

    oct

---
+++ October4MorningExercise/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String personName = "John Doe";
+        int personAge = 25;
+        double productPrice = 19.99;
+        float temperatureCelcius = 27.5f;
+        boolean isLoggedIn = true;
+    }
+}
