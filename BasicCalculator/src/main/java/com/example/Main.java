048a2a66922d098b2a43cedc25348d412d450061
Supriya Roberson <supriyaroberson@Supriyas-MBP.home>
2023-10-04 16:01:36 -0500

    Completed Mod3 - Exercise 3 - Basic Calculator

---
+++ BasicCalculator/src/main/java/com/example/Main.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give me the first number:");
+        double firstNumber = scanner.nextDouble();
+
+        System.out.println("Give me the second number:");
+        double secondNumber = scanner.nextDouble();
+
+        System.out.println("What would you like to do?");
+        String option = scanner.nextLine();
+
+        String joinNumbers = scanner.nextLine();
+
+        if (joinNumbers.equalsIgnoreCase("M")){
+            System.out.println(firstNumber * secondNumber);
+        }
+        else if (joinNumbers.equalsIgnoreCase("A")){
+            System.out.println(firstNumber + secondNumber);
+        }
+        else if (joinNumbers.equalsIgnoreCase("D")){
+            System.out.println(firstNumber / secondNumber);
+        }
+        else if (joinNumbers.equalsIgnoreCase("S")){
+            System.out.println(firstNumber - secondNumber);
+        }
+
+    }
+    }
