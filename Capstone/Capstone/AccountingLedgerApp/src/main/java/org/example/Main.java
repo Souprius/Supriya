4f95e44ca8255c33a37771ffc6edc104573510b8
Souprius <146770154+Souprius@users.noreply.github.com>
2023-10-26 11:30:05 -0500

    Re-uploading adjustments

---
+++ Capstone/Capstone/AccountingLedgerApp/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Supriya Federal Credit Union");
+
+       UserInterface ui = new UserInterface();
+       ui.homeOptions();
+
+    }
+}
